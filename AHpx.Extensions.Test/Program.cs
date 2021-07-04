c8fbb56526ada034db81bfeb46bf1c2b220ddca2
AHpx <AHpx@yandex.com>
2021-07-05 00:11:32 +0800

    Initialize project

---
+++ AHpx.Extensions.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AHpx.Extensions.Test
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
