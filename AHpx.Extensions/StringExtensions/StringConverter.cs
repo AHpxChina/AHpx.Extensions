b979a358fa6eb1364fd7fdbdaea664fa127cc44e
AHpx <agpx@yandex.com>
2021-07-05 18:56:10 +0800

    Add string extensions

---
+++ AHpx.Extensions/StringExtensions/StringConverter.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace AHpx.Extensions.StringExtensions
+{
+    public static class StringConverter
+    {
+        public static JObject ToJObject(this string s)
+        {
+            s = s.IsNotNullOrEmptyThrow("A null or empty string can't be convert to json!")
+                .IsJsonStringOrThrow("Incoming string is not a valid json string!");
+            
+            return JObject.Parse(s);
+        }
+    }
+}
