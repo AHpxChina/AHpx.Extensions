06411c1cedd2ec72601c488dae15709b4b0f6d2c
AHpx <AHpx@yandex.com>
2021-07-05 22:45:50 +0800

    Add StringOperator.cs

---
+++ AHpx.Extensions/StringExtensions/StringOperator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AHpx.Extensions.StringExtensions
+{
+    public static class StringOperator
+    {
+        /// <summary>
+        /// Empty specify content in string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static string Empty(this string s, string target)
+        {
+            return s.Replace(target, string.Empty);
+        }
+
+        /// <summary>
+        /// Empty all the linebreak symbol in string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string EmptyLinebreak(this string s)
+        {
+            return s.Empty(Environment.NewLine);
+        }
+    }
+}
