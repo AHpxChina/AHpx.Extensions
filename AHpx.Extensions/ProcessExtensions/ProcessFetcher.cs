695be67a08ee294fb78f7f7daf05b01bc8de94a3
AHpx <AHpx@yandex.com>
2021-07-05 22:51:06 +0800

    Add ProcessFetcher.cs

---
+++ AHpx.Extensions/ProcessExtensions/ProcessFetcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AHpx.Extensions.ProcessExtensions
+{
+    public static class ProcessFetcher
+    {
+        public static string ReadOutputLine(this Process process)
+        {
+            return process.StandardOutput.ReadLine();
+        }
+
+        public static async Task<string> ReadOutputLineAsync(this Process process)
+        {
+            return await process.StandardOutput.ReadLineAsync();
+        }
+    }
+}
