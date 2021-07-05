81fb2d04b764049e1e321940242f4d3ce93d4c14
AHpx <AHpx@yandex.com>
2021-07-05 23:04:15 +0800

    Add ProcessUtilities.cs

--- AHpx.Extensions.Test/Program.cs
+++ AHpx.Extensions.Test/Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using AHpx.Extensions.IOExtensions;
 using AHpx.Extensions.JsonExtensions;
 using AHpx.Extensions.StringExtensions;
+using AHpx.Extensions.Utils;
 using Newtonsoft.Json;
 
 namespace AHpx.Extensions.Test
 {
     internal static class Program
     {
-        private static async Task Main(string[] args)
+        private static void Main(string[] args)
         {
-            var filePath = new DirectoryInfo(@"C:\Users\ahpx\Desktop");
-
-            Console.WriteLine(filePath.GetSubFile("text.txt"));
-            Console.WriteLine(filePath.GetSubDirectory("Hypixel").Exists);
+            Console.WriteLine(ProcessUtilities.IsProcessRunning("javaw"));
+            Console.WriteLine(ProcessUtilities.IsProcessRunning("qq"));
         }
     }
     
---
+++ AHpx.Extensions/Utils/ProcessUtilities.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AHpx.Extensions.Utils
+{
+    public static class ProcessUtilities
+    {
+        /// <summary>
+        /// Construct a standard process object
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="arguments"></param>
+        /// <param name="workingDirectory"></param>
+        /// <returns></returns>
+        public static Process ConstructStandardProcess(string fileName, string arguments = null, string workingDirectory = null)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = fileName,
+                    Arguments = arguments ?? string.Empty,
+                    WorkingDirectory = workingDirectory ?? string.Empty,
+                    UseShellExecute = false,
+                    RedirectStandardInput = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                },
+                EnableRaisingEvents = true
+            };
+
+            return process;
+        }
+
+        /// <summary>
+        /// Get main window titles of specify process name
+        /// </summary>
+        /// <param name="processName">process name without extension name</param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetWindowTitle(string processName)
+        {
+            var processes = Process.GetProcessesByName(processName);
+
+            foreach (var process in processes)
+            {
+                yield return process.MainWindowTitle;
+            }
+        }
+
+        /// <summary>
+        /// Determine if process is running
+        /// </summary>
+        /// <param name="processName">process name without extension name</param>
+        /// <returns></returns>
+        public static bool IsProcessRunning(string processName)
+        {
+            return Process.GetProcessesByName(processName).Length > 0;
+        }
+    }
+}
